b9d34583de469c4f0cb2cd225ba71d282a5a640c
Chandrakanta Haransingh <chandrakantaharansingh@Chandrakantas-MacBook-Air.local>
2022-06-08 23:21:27 +0530

    inovice changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,13 +14,14 @@ class DashboardController extends Controller
     }
 
     public function dashboard(){
-        $allusers=User::get();
+        $allusers=User::where('is_delete',0)->get();
         $alluserscount=count($allusers);
         $first_day_this_month = date('01-m-Y'); // hard-coded '01' for first day
         $last_day_this_month  = date('t-m-Y');
         $month = date('m', strtotime(date('d-m-Y', time())));
         $thismonthusers = DB::table('users')
            ->whereMonth('created_at', $month)
+           ->where('is_delete',0)
            ->count();
         $totalrevenue = DB::table('fees')->where('status','paid')->sum('fees');
         $amounttobecollected = DB::table('fees')->where('status','unpaid')->sum('fees');
